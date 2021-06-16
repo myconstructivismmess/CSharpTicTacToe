7ca6c7a7bb4cf0e2c9f8ed26885a1559c8506f10
tomrouet <rouettom@gmail.com>
2021-06-16 11:25:08 +0200

    Initialization

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            /*Dictionary<String, String> game = new Dictionary<string, string>()
+            {
+                {}
+            }*/
+            Console.WriteLine("Hello world !");
+            Console.ReadLine();
+        }
+    }
+}
